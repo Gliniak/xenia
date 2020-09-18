2c068cefbc13c348f31c2a085f8980be8511b1df
Gliniak <Gliniak93@gmail.com>
2020-09-18 19:40:31 +0200

    [Kernel/Usbcam] Added stubs for:  - XUsbcamReadFrame  - XUsbcamSetConfig  - XUsbcamSetCaptureMode
    Described states of XUsbcamGetState
    Returning dummy handle from XUsbcamCreate

--- src/xenia/kernel/xboxkrnl/xboxkrnl_usbcam.cc
+++ src/xenia/kernel/xboxkrnl/xboxkrnl_usbcam.cc
@@ -19,7 +19,7 @@ namespace xboxkrnl {
 
 dword_result_t XUsbcamCreate(dword_t buffer,
                              dword_t buffer_size,  // 0x4B000 640x480?
-                             lpunknown_t unk3_ptr) {
+                             lpdword_t handle_out) {
   // This function should return success.
   // It looks like it only allocates space for usbcam support.
   // returning error code might cause games to initialize incorrectly.
@@ -27,15 +27,37 @@ dword_result_t XUsbcamCreate(dword_t buffer,
   // function. If value is different than 0 instead of loading
   // rest of the game it returns from initalization function and tries
   // to run game normally which causes crash, due to uninitialized data.
+  kernel_memory()->Fill(buffer, buffer_size, 0);
+
+  *handle_out = 0xDEADF00D;
   return X_STATUS_SUCCESS;
 }
 DECLARE_XBOXKRNL_EXPORT1(XUsbcamCreate, kNone, kStub);
 
 dword_result_t XUsbcamGetState() {
-  // 0 = not connected.
+  // 0 - Not connected.
+  // 1 - Plugged in
+  // 2 - Initialized
   return 0;
 }
 DECLARE_XBOXKRNL_EXPORT1(XUsbcamGetState, kNone, kStub);
+
+dword_result_t XUsbcamReadFrame(dword_t handle, dword_t buffer, dword_t buffer_size,
+                                dword_t r6, dword_t overlapped_ptr) {
+  return X_ERROR_IO_PENDING;
+}
+DECLARE_XBOXKRNL_EXPORT1(XUsbcamReadFrame, kNone, kStub);
+
+dword_result_t XUsbcamSetConfig(dword_t r3, dword_t r4, dword_t r5, dword_t r6,
+                                dword_t r7, dword_t r8, dword_t r9) {
+  return X_STATUS_SUCCESS;
+}
+DECLARE_XBOXKRNL_EXPORT1(XUsbcamSetConfig, kNone, kStub);
+
+dword_result_t XUsbcamSetCaptureMode(dword_t handle) {
+  return X_STATUS_SUCCESS;
+}
+DECLARE_XBOXKRNL_EXPORT1(XUsbcamSetCaptureMode, kNone, kStub);
 
 void RegisterUsbcamExports(xe::cpu::ExportResolver* export_resolver,
                            KernelState* kernel_state) {}
