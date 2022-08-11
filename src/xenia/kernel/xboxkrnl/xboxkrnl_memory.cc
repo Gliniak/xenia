93758557f25891672521bdb6aebab63c09236416
Gliniak <Gliniak93@gmail.com>
2022-08-11 21:30:32 +0200

    [Kernel/Memory] Round adjusted_size to 64k page when base is unspecified

--- src/xenia/kernel/xboxkrnl/xboxkrnl_memory.cc
+++ src/xenia/kernel/xboxkrnl/xboxkrnl_memory.cc
@@ -120,7 +120,8 @@ dword_result_t NtAllocateVirtualMemory_entry(lpdword_t base_addr_ptr,
   uint32_t adjusted_size = int32_t(*region_size_ptr) < 0
                                ? -int32_t(region_size_ptr.value())
                                : region_size_ptr.value();
-  adjusted_size = xe::round_up(adjusted_size, page_size);
+  adjusted_size =
+      xe::round_up(adjusted_size, adjusted_base ? page_size : 64 * 1024);
 
   // Allocate.
   uint32_t allocation_type = 0;
