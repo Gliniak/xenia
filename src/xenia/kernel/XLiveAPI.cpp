AdrianCassar/xenia-canary

[Kernel] Fix LIVE notifications

src/xenia/kernel/XLiveAPI.cpp
@@ -210,6 +210,10 @@ void XLiveAPI::Init() {
     // Assign online ip as local ip to ensure XNADDR is not 0 for systemlink
     online_ip_ = local_ip_;
 
+    // Fixes 4D53085F from crashing when joining via systemlink.
+    kernel_state()->BroadcastNotification(kXNotificationLiveConnectionChanged,
+                                          X_ONLINE_S_LOGON_DISCONNECTED);
+
     XELOGE("XLiveAPI:: Cannot reach API server.");
     initialized_ = InitState::Failed;
     return;

src/xenia/kernel/kernel_state.cc
@@ -38,6 +38,8 @@ DEFINE_uint32(kernel_build_version, 1888, "Define current kernel version",
 
 DECLARE_string(cl);
 
+DECLARE_bool(offline_mode);
+
 namespace xe {
 namespace kernel {
 
@@ -886,11 +888,18 @@ void KernelState::RegisterNotifyListener(XNotifyListener* listener) {
 
     listener->EnqueueNotification(kXNotificationDvdDriveTrayStateChanged,
                                   X_DVD_DISC_STATE::XBOX_360_GAME_DISC);
+  }
+
+  if (listener->mask() & kXNotifyLive) {
+    uint32_t live_connection_state =
+        cvars::offline_mode ? X_ONLINE_S_LOGON_DISCONNECTED
+                            : X_ONLINE_S_LOGON_CONNECTION_ESTABLISHED;
+    uint32_t ethernet_link_state = cvars::offline_mode ? 0 : 1;
 
     listener->EnqueueNotification(kXNotificationLiveConnectionChanged,
-                                  X_ONLINE_S_LOGON_CONNECTION_ESTABLISHED);
-    listener->EnqueueNotification(kXNotificationLiveConnectionChanged,
-                                  1);  // Ethernet Enabled
+                                  live_connection_state);
+    listener->EnqueueNotification(kXNotificationLiveLinkStateChanged,
+                                  ethernet_link_state);
   }
 }
 

Gliniak/xenia

src/xenia/kernel/XLiveAPI.cpp
@@ -210,6 +210,10 @@ void XLiveAPI::Init() {
     // Assign online ip as local ip to ensure XNADDR is not 0 for systemlink
     online_ip_ = local_ip_;
 
+    // Fixes 4D53085F from crashing when joining via systemlink.
+    kernel_state()->BroadcastNotification(kXNotificationIDLiveConnectionChanged,
+                                          X_ONLINE_S_LOGON_DISCONNECTED);
+
     XELOGE("XLiveAPI:: Cannot reach API server.");
     initialized_ = InitState::Failed;
     return;

src/xenia/kernel/kernel_state.cc
@@ -46,6 +46,8 @@ DEFINE_uint32(kernel_build_version, 1888, "Define current kernel version",
 
 DECLARE_string(cl);
 
+DECLARE_bool(offline_mode);
+
 namespace xe {
 namespace kernel {
 
@@ -883,11 +885,19 @@ void KernelState::RegisterNotifyListener(XNotifyListener* listener) {
     // XN_SYS_SIGNINCHANGED x2
     listener->EnqueueNotification(kXNotificationIDSystemSignInChanged, 1);
     listener->EnqueueNotification(kXNotificationIDSystemSignInChanged, 1);
-    // LIVE
+  }
+
+  // LIVE
+  if (listener->mask() & kXNotifyLive) {
+    uint32_t live_connection_state =
+        cvars::offline_mode ? X_ONLINE_S_LOGON_DISCONNECTED
+                            : X_ONLINE_S_LOGON_CONNECTION_ESTABLISHED;
+    uint32_t ethernet_link_state = cvars::offline_mode ? 0 : 1;
+
     listener->EnqueueNotification(kXNotificationIDLiveConnectionChanged,
-                                  X_ONLINE_S_LOGON_CONNECTION_ESTABLISHED);
-    listener->EnqueueNotification(kXNotificationIDLiveConnectionChanged,
-                                  1);  // Ethernet Enabled
+                                  live_connection_state);
+    listener->EnqueueNotification(kXNotificationIDLiveLinkStateChanged,
+                                  ethernet_link_state);
   }
 }
 