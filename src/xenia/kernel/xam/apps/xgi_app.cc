OpenCombas/xenia-canary

[XLast] Partially implemented presence string parsing
Thanks Gliniak for parser.

src/xenia/kernel/xam/apps/xgi_app.cc
@@ -472,6 +472,10 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
         kernel_state_->xam_state()->user_tracker()->UpdateContext(
             user->xuid(), xgi_context->context.context_id,
             xgi_context->context.value);
+
+        if (xgi_context->context.context_id == X_CONTEXT_PRESENCE) {
+          auto presence = user->GetPresenceString();
+        }
       }
       return X_E_SUCCESS;
     }

Gliniak/xenia

src/xenia/kernel/xam/apps/xgi_app.cc
@@ -419,6 +419,10 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
         UserProfile* user_profile = kernel_state_->user_profile(user_index);
         if (user_profile) {
           user_profile->contexts_[context_id] = context_value;
+
+          if (context_id == X_CONTEXT_PRESENCE) {
+            auto presence = user_profile->GetPresenceString();
+          }
         }
       }
       return X_E_SUCCESS;