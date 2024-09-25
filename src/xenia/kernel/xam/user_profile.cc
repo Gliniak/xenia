AdrianCassar/xenia-canary

[XAM] Implemented basic friends support

src/xenia/kernel/xam/user_profile.cc
@@ -21,6 +21,8 @@
 #include "xenia/kernel/util/shim_utils.h"
 #include "xenia/kernel/xam/xdbf/gpd_info.h"
 
+#include "xenia/kernel/XLiveAPI.h"
+
 namespace xe {
 namespace kernel {
 namespace xam {
@@ -35,6 +37,12 @@ UserProfile::UserProfile(const uint64_t xuid,
 
   LoadProfileIcon(XTileType::kGamerTile);
   LoadProfileIcon(XTileType::kGamerTileSmall);
+
+  friends_ = std::vector<X_ONLINE_FRIEND>();
+
+  for (const auto& friend_xuid : XLiveAPI::ParseFriendsXUIDs()) {
+    AddFriendFromXUID(friend_xuid);
+  }
 }
 
 GpdInfo* UserProfile::GetGpd(const uint32_t title_id) {
@@ -186,6 +194,209 @@ bool UserProfile::WriteGpd(const uint32_t title_id) {
                   &written_bytes);
   file->Destroy();
   return true;
+}
+
+X_ONLINE_FRIEND UserProfile::GenerateDummyFriend() {
+  std::random_device rnd;
+  std::mt19937_64 gen(rnd());
+  std::uniform_int_distribution<int> dist(0x00, 0xFF);
+
+  X_ONLINE_FRIEND dummy_friend = {};
+
+  // Friend is playing same title
+  dummy_friend.title_id = kernel_state()->title_id();
+
+  const uint32_t player_state = X_ONLINE_FRIENDSTATE_FLAG_ONLINE |
+                                X_ONLINE_FRIENDSTATE_FLAG_JOINABLE |
+                                X_ONLINE_FRIENDSTATE_FLAG_PLAYING;
+
+  const uint32_t user_state = X_ONLINE_FRIENDSTATE_ENUM_ONLINE;
+
+  dummy_friend.xuid =
+      kernel_state()->xam_state()->profile_manager()->GenerateXuidOnline();
+  dummy_friend.session_id = XNKID();
+  dummy_friend.state = player_state | user_state;
+
+  xe::be<uint64_t> session_id = 0xAE00FFFFFFFFFFFF;
+  memcpy(dummy_friend.session_id.ab, &session_id, sizeof(XNKID));
+
+  // uint64_t xnkidInvite = 0xAE00FFFFFFFFFFFF;
+  // memcpy(dummy_friend.xnkidInvite.ab, &xnkidInvite, sizeof(XNKID));
+
+  std::string gamertag = fmt::format("Player {}", dist(gen));
+  std::u16string rich_presence = u"Playing on Xenia";
+
+  char* gamertag_ptr = reinterpret_cast<char*>(dummy_friend.Gamertag);
+  strcpy(gamertag_ptr, gamertag.c_str());
+
+  char16_t* rich_presence_ptr =
+      reinterpret_cast<char16_t*>(dummy_friend.wszRichPresence);
+  xe::string_util::copy_and_swap_truncating(
+      rich_presence_ptr, rich_presence, sizeof(dummy_friend.wszRichPresence));
+
+  dummy_friend.cchRichPresence =
+      static_cast<uint32_t>(rich_presence.size() * sizeof(char16_t));
+
+  return dummy_friend;
+}
+
+void UserProfile::AddDummyFriends(const uint32_t friends_count) {
+  if (friends_.size() >= X_ONLINE_MAX_FRIENDS) {
+    return;
+  }
+
+  for (uint32_t i = 0; i < friends_count; i++) {
+    X_ONLINE_FRIEND peer = GenerateDummyFriend();
+
+    AddFriend(&peer);
+  }
+}
+
+bool UserProfile::GetFriendPresenceFromXUID(const uint64_t xuid,
+                                            X_ONLINE_PRESENCE* presence) {
+  if (presence == nullptr) {
+    return false;
+  }
+
+  X_ONLINE_FRIEND peer = {};
+
+  const bool is_friend = GetFriendFromXUID(xuid, &peer);
+
+  if (!is_friend) {
+    return false;
+  }
+
+  presence->title_id = peer.title_id;
+  presence->state = peer.state;
+  presence->xuid = peer.xuid;
+  presence->session_id = peer.session_id;
+  presence->cchRichPresence = peer.cchRichPresence;
+
+  memcpy(presence->wszRichPresence, peer.wszRichPresence,
+         presence->cchRichPresence);
+
+  return true;
+}
+
+bool UserProfile::SetFriend(const X_ONLINE_FRIEND& update_peer) {
+  auto it = std::find_if(
+      friends_.begin(), friends_.end(), [&update_peer](X_ONLINE_FRIEND& peer) {
+        if (peer.xuid == update_peer.xuid) {
+          memcpy(&peer, &update_peer, sizeof(X_ONLINE_FRIEND));
+          return true;
+        }
+
+        return false;
+      });
+
+  if (it != friends_.end()) {
+    return false;
+  }
+
+  return true;
+}
+
+bool UserProfile::AddFriendFromXUID(const uint64_t xuid) {
+  X_ONLINE_FRIEND peer = X_ONLINE_FRIEND();
+  peer.xuid = xuid;
+
+  return AddFriend(&peer);
+}
+
+bool UserProfile::AddFriend(X_ONLINE_FRIEND* peer) {
+  if (friends_.size() >= X_ONLINE_MAX_FRIENDS) {
+    return false;
+  }
+
+  if (peer == nullptr) {
+    return false;
+  }
+
+  if (IsFriend(peer->xuid)) {
+    return true;
+  }
+
+  std::string default_gamertag = fmt::format("{:016X}", peer->xuid.get());
+
+  XELOGI("{}: Added gamertag: {}", __func__, default_gamertag);
+
+  strcpy(peer->Gamertag, default_gamertag.c_str());
+
+  friends_.push_back(*peer);
+
+  return true;
+}
+
+bool UserProfile::RemoveFriend(const X_ONLINE_FRIEND& peer) {
+  return RemoveFriend(peer.xuid);
+}
+
+bool UserProfile::RemoveFriend(const uint64_t xuid) {
+  bool removed = false;
+
+  auto it = std::remove_if(
+      friends_.begin(), friends_.end(),
+      [&xuid](const X_ONLINE_FRIEND& peer) { return peer.xuid == xuid; });
+
+  if (it != friends_.end()) {
+    const size_t friends_size = friends_.size();
+
+    friends_.erase(it, friends_.end());
+    removed = friends_.size() != friends_size;
+  }
+
+  return removed;
+}
+
+bool UserProfile::GetFriendFromIndex(const uint32_t index,
+                                     X_ONLINE_FRIEND* peer) {
+  if (index >= X_ONLINE_MAX_FRIENDS || index >= friends_.size()) {
+    return false;
+  }
+
+  if (peer == nullptr) {
+    return false;
+  }
+
+  memcpy(peer, &friends_[index], sizeof(X_ONLINE_FRIEND));
+
+  return true;
+}
+
+bool UserProfile::GetFriendFromXUID(const uint64_t xuid,
+                                    X_ONLINE_FRIEND* peer) {
+  if (peer == nullptr) {
+    return false;
+  }
+
+  return IsFriend(xuid, peer);
+}
+
+bool UserProfile::IsFriend(const uint64_t xuid, X_ONLINE_FRIEND* peer) {
+  auto it = std::find_if(
+      friends_.begin(), friends_.end(),
+      [&xuid](const X_ONLINE_FRIEND& peer) { return peer.xuid == xuid; });
+
+  if (it == friends_.end()) {
+    return false;
+  }
+
+  if (peer != nullptr) {
+    memcpy(peer, &*it, sizeof(X_ONLINE_FRIEND));
+  }
+
+  return true;
+}
+
+const std::vector<uint64_t> UserProfile::GetFriendsXUIDs() const {
+  std::vector<uint64_t> xuids;
+  xuids.reserve(friends_.size());
+
+  for (const auto& peer : friends_) {
+    xuids.push_back(peer.xuid);
+  }
+
+  return xuids;
 }
 
 }  // namespace xam

Gliniak/xenia

src/xenia/kernel/xam/user_profile.cc
@@ -59,10 +59,16 @@ UserProfile::UserProfile(uint8_t index) {
   //  name_ = "User_" + std::to_string(index);
   //}
 
+  friends_ = std::vector<X_ONLINE_FRIEND>();
+
   index_ = index;
 
   switch (index) {
     case 0: {
+      for (const auto& xuid : XLiveAPI::ParseFriendsXUIDs()) {
+        AddFriendFromXUID(xuid);
+      }
+
       // If XUID is empty generate another one.
       if (cvars::user_0_xuid.empty()) {
         OVERRIDE_string(user_0_xuid,
@@ -228,6 +234,208 @@ UserProfile::UserProfile(uint8_t index) {
   AddSetting(std::make_unique<UserSetting>(0x63E83FFE, std::vector<uint8_t>()));
   // XPROFILE_TITLE_SPECIFIC3
   AddSetting(std::make_unique<UserSetting>(0x63E83FFD, std::vector<uint8_t>()));
+}
+
+X_ONLINE_FRIEND UserProfile::GenerateDummyFriend() {
+  std::random_device rnd;
+  std::mt19937_64 gen(rnd());
+  std::uniform_int_distribution<int> dist(0x00, 0xFF);
+
+  X_ONLINE_FRIEND dummy_friend = {};
+
+  // Friend is playing same title
+  dummy_friend.title_id = kernel_state()->title_id();
+
+  const uint32_t player_state = X_ONLINE_FRIENDSTATE_FLAG_ONLINE |
+                                X_ONLINE_FRIENDSTATE_FLAG_JOINABLE |
+                                X_ONLINE_FRIENDSTATE_FLAG_PLAYING;
+
+  const uint32_t user_state = X_ONLINE_FRIENDSTATE_ENUM_ONLINE;
+
+  dummy_friend.xuid = GenerateOnlineXUID();
+  dummy_friend.session_id = XNKID();
+  dummy_friend.state = player_state | user_state;
+
+  xe::be<uint64_t> session_id = 0xAE00FFFFFFFFFFFF;
+  memcpy(dummy_friend.session_id.ab, &session_id, sizeof(XNKID));
+
+  // uint64_t xnkidInvite = 0xAE00FFFFFFFFFFFF;
+  // memcpy(dummy_friend.xnkidInvite.ab, &xnkidInvite, sizeof(XNKID));
+
+  std::string gamertag = fmt::format("Player {}", dist(gen));
+  std::u16string rich_presence = u"Playing on Xenia";
+
+  char* gamertag_ptr = reinterpret_cast<char*>(dummy_friend.Gamertag);
+  strcpy(gamertag_ptr, gamertag.c_str());
+
+  char16_t* rich_presence_ptr =
+      reinterpret_cast<char16_t*>(dummy_friend.wszRichPresence);
+  xe::string_util::copy_and_swap_truncating(
+      rich_presence_ptr, rich_presence, sizeof(dummy_friend.wszRichPresence));
+
+  dummy_friend.cchRichPresence =
+      static_cast<uint32_t>(rich_presence.size() * sizeof(char16_t));
+
+  return dummy_friend;
+}
+
+void UserProfile::AddDummyFriends(const uint32_t friends_count) {
+  if (friends_.size() >= X_ONLINE_MAX_FRIENDS) {
+    return;
+  }
+
+  for (uint32_t i = 0; i < friends_count; i++) {
+    X_ONLINE_FRIEND peer = GenerateDummyFriend();
+
+    AddFriend(&peer);
+  }
+}
+
+bool UserProfile::GetFriendPresenceFromXUID(const uint64_t xuid,
+                                            X_ONLINE_PRESENCE* presence) {
+  if (presence == nullptr) {
+    return false;
+  }
+
+  X_ONLINE_FRIEND peer = {};
+
+  const bool is_friend = GetFriendFromXUID(xuid, &peer);
+
+  if (!is_friend) {
+    return false;
+  }
+
+  presence->title_id = peer.title_id;
+  presence->state = peer.state;
+  presence->xuid = peer.xuid;
+  presence->session_id = peer.session_id;
+  presence->cchRichPresence = peer.cchRichPresence;
+
+  memcpy(presence->wszRichPresence, peer.wszRichPresence,
+         presence->cchRichPresence);
+
+  return true;
+}
+
+bool UserProfile::SetFriend(const X_ONLINE_FRIEND& update_peer) {
+  auto it = std::find_if(
+      friends_.begin(), friends_.end(), [&update_peer](X_ONLINE_FRIEND& peer) {
+        if (peer.xuid == update_peer.xuid) {
+          memcpy(&peer, &update_peer, sizeof(X_ONLINE_FRIEND));
+          return true;
+        }
+
+        return false;
+      });
+
+  if (it != friends_.end()) {
+    return false;
+  }
+
+  return true;
+}
+
+bool UserProfile::AddFriendFromXUID(const uint64_t xuid) {
+  X_ONLINE_FRIEND peer = X_ONLINE_FRIEND();
+  peer.xuid = xuid;
+
+  return AddFriend(&peer);
+}
+
+bool UserProfile::AddFriend(X_ONLINE_FRIEND* peer) {
+  if (friends_.size() >= X_ONLINE_MAX_FRIENDS) {
+    return false;
+  }
+
+  if (peer == nullptr) {
+    return false;
+  }
+
+  if (IsFriend(peer->xuid)) {
+    return true;
+  }
+
+  std::string default_gamertag = fmt::format("{:016X}", peer->xuid);
+
+  XELOGI("{}: Added gamertag: {}", __func__, default_gamertag);
+
+  strcpy(peer->Gamertag, default_gamertag.c_str());
+
+  friends_.push_back(*peer);
+
+  return true;
+}
+
+bool UserProfile::RemoveFriend(const X_ONLINE_FRIEND& peer) {
+  return RemoveFriend(peer.xuid);
+}
+
+bool UserProfile::RemoveFriend(const uint64_t xuid) {
+  bool removed = false;
+
+  auto it = std::remove_if(
+      friends_.begin(), friends_.end(),
+      [&xuid](const X_ONLINE_FRIEND& peer) { return peer.xuid == xuid; });
+
+  if (it != friends_.end()) {
+    const size_t friends_size = friends_.size();
+
+    friends_.erase(it, friends_.end());
+    removed = friends_.size() != friends_size;
+  }
+
+  return removed;
+}
+
+bool UserProfile::GetFriendFromIndex(const uint32_t index,
+                                     X_ONLINE_FRIEND* peer) {
+  if (index >= X_ONLINE_MAX_FRIENDS || index >= friends_.size()) {
+    return false;
+  }
+
+  if (peer == nullptr) {
+    return false;
+  }
+
+  memcpy(peer, &friends_[index], sizeof(X_ONLINE_FRIEND));
+
+  return true;
+}
+
+bool UserProfile::GetFriendFromXUID(const uint64_t xuid,
+                                    X_ONLINE_FRIEND* peer) {
+  if (peer == nullptr) {
+    return false;
+  }
+
+  return IsFriend(xuid, peer);
+}
+
+bool UserProfile::IsFriend(const uint64_t xuid, X_ONLINE_FRIEND* peer) {
+  auto it = std::find_if(
+      friends_.begin(), friends_.end(),
+      [&xuid](const X_ONLINE_FRIEND& peer) { return peer.xuid == xuid; });
+
+  if (it == friends_.end()) {
+    return false;
+  }
+
+  if (peer != nullptr) {
+    memcpy(peer, &*it, sizeof(X_ONLINE_FRIEND));
+  }
+
+  return true;
+}
+
+const std::vector<uint64_t> UserProfile::GetFriendsXUIDs() const {
+  std::vector<uint64_t> xuids;
+  xuids.reserve(friends_.size());
+
+  for (const auto& peer : friends_) {
+    xuids.push_back(peer.xuid);
+  }
+
+  return xuids;
 }
 
 void UserProfile::AddSetting(std::unique_ptr<UserSetting> setting) {
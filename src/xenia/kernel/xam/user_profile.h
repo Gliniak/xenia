3266c06f998772ec68f568832df8a3c440a7cfc1
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-09-09 21:24:27 +0100

    [XAM] Implemented basic friends support

--- src/xenia/kernel/xam/user_profile.h
+++ src/xenia/kernel/xam/user_profile.h
@@ -19,6 +19,7 @@
 
 #include "xenia/base/byte_stream.h"
 #include "xenia/kernel/util/xuserdata.h"
+#include "xenia/kernel/xnet.h"
 #include "xenia/xbox.h"
 
 DECLARE_bool(offline_mode);
@@ -214,6 +215,26 @@ class UserProfile {
            static_cast<uint32_t>(X_USER_SIGNIN_STATE::SignedInToLive);
   }
 
+  static X_ONLINE_FRIEND GenerateDummyFriend();
+
+  void AddDummyFriends(const uint32_t friends_count);
+
+  bool GetFriendPresenceFromXUID(const uint64_t xuid,
+                                 X_ONLINE_PRESENCE* presence);
+
+  bool SetFriend(const X_ONLINE_FRIEND& update_peer);
+  bool AddFriendFromXUID(const uint64_t xuid);
+  bool AddFriend(X_ONLINE_FRIEND* add_friend);
+  bool RemoveFriend(const X_ONLINE_FRIEND& peer);
+  bool RemoveFriend(const uint64_t xuid);
+
+  bool GetFriendFromIndex(const uint32_t index, X_ONLINE_FRIEND* peer);
+  bool GetFriendFromXUID(const uint64_t xuid, X_ONLINE_FRIEND* peer);
+  bool IsFriend(const uint64_t xuid, X_ONLINE_FRIEND* peer = nullptr);
+
+  const std::vector<X_ONLINE_FRIEND> GetFriends() const { return friends_; }
+  const std::vector<uint64_t> GetFriendsXUIDs() const;
+
   void AddSetting(std::unique_ptr<UserSetting> setting);
   UserSetting* GetSetting(uint32_t setting_id);
 
@@ -225,6 +246,7 @@ class UserProfile {
   std::string name_;
   std::vector<std::unique_ptr<UserSetting>> setting_list_;
   std::unordered_map<uint32_t, UserSetting*> settings_;
+  std::vector<X_ONLINE_FRIEND> friends_;
 
   void LoadSetting(UserSetting*);
   void SaveSetting(UserSetting*);
